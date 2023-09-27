2ba3a4994dbebe1e6019f692b7e5984e0dad9598
Artemis <Chef1991@gmail.com>
2023-09-26 20:30:42 -0700

    Refactor build to use new sketch file structure
    * Change ketches to use exported structure from the p5 web site
    * Add index page with a list of sketches

--- build/build.py
+++ build/build.py
@@ -2,12 +2,22 @@ import os
 import argparse
 import pathlib
 import jinja2
+from dataclasses import dataclass
+import yaml
+import shutil
 
-
+@dataclass
+class Sketch:
+    name: str
+    description: str
+    href: str
+    
+    
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--out-dir', required=True, help='Name of the output directory, must not exist')
     parser.add_argument('--template-dir', default='templates', help='path to the templates directory')
+    parser.add_argument('--clean', action='store_true', help='clean the output directory if it exists')
     parser.add_argument('sketch_dir', help="sketch directory")
 
     args = parser.parse_args()
@@ -15,6 +25,7 @@ def main():
     out_dir_path = args.out_dir
     template_dir_path = args.template_dir
     sketch_dir_path = args.sketch_dir
+    clean = args.clean
 
     print(f"{out_dir_path=}")
     print(f"{template_dir_path=}")
@@ -28,11 +39,18 @@ def main():
     script_template_file = template_dir / "sketch.js.j2"
     html_template_file = template_dir / "sketch.html.j2"
 
+    index_template_file = template_dir / "index.html.j2"
+
     if out_dir.exists():
-        print("error: out dir already exists")
-        exit(1)
+        if clean:
+            print("warn: out dir exists, deleting")
+            shutil.rmtree(out_dir)
+        else:
+            print("error: out dir already exists")
+            exit(1)
 
     out_dir.mkdir()
+
 
 
     with script_template_file.open('r') as f:
@@ -40,7 +58,50 @@ def main():
     with html_template_file.open('r') as f:
         html_template = f.read()
     
+    with index_template_file.open('r') as f:
+        index_template_src = f.read()
+
+
     out_sketch_dir_name = "sketches"
+
+    # go throug the sketch dir and build a list of sketch objects
+    # then template the index.html file from the list of objects
+    sketches = []
+    for f in sketch_dir.iterdir():
+        print(f"{f=}")
+        if not f.is_dir():
+            continue            
+        print("f.name", f.name)
+        sketch_href = f"{out_sketch_dir_name}/{f.name}/index.html"
+        print(f"{sketch_href=}")
+        sketch_info_file = f / "sketch.yaml"
+        sketch_name = None
+        sketch_description = None
+        if sketch_info_file.exists():
+            with sketch_info_file.open('r') as f:
+                sketch_info = yaml.safe_load(f)
+                if (temp_name := sketch_info.get('name')):
+                    sketch_name = temp_name
+                if (temp_description := sketch_info.get('description')):
+                    sketch_description = temp_description
+        if not sketch_name:
+            sketch_name = f.name
+        if not sketch_description:
+            sketch_description = ""
+        sketch = Sketch(name=sketch_name, description=sketch_description, href=sketch_href)
+        sketches.append(sketch)
+        print("------------")
+    
+    
+    index_template = jinja2.Template(index_template_src)
+
+    html_out = index_template.render(sketches=sketches)
+    out_file_path = out_dir / "index.html"
+    with out_file_path.open('w') as f:
+        f.write(html_out)
+    
+
+    exit()
 
     for sk_file in sketch_dir.iterdir():
         file_name = sk_file.name
